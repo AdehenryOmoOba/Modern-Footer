5a734aeffe2798e3491b80a63e38669c5adf9f50
Adefemi <adefemiadelaja@gmail.com>
2021-01-13 18:28:08 +0100

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+const bg = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  bg.style.backgroundSize = 160 - +window.pageYOffset / 12 + "%";
+  //   bg.style.opacity = 1 - +window.pageYOffset / 700 + "";
+});
