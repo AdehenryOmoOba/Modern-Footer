131bd7316830a6dbe2dfe866e1b3439cd6664a81
Adefemi <adefemiadelaja@gmail.com>
2021-01-14 20:11:19 +0100

    Adehenry

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const bg = document.querySelector(".header");
+const hamburger = document.querySelector(".toggle");
+const menu = document.querySelectorAll(".item");
 
 window.addEventListener("scroll", () => {
   bg.style.backgroundSize = 160 - +window.pageYOffset / 12 + "%";
   //   bg.style.opacity = 1 - +window.pageYOffset / 700 + "";
+});
+
+hamburger.addEventListener("click", () => {
+  menu.forEach((item) => {
+    item.classList.toggle("active");
+  });
 });
